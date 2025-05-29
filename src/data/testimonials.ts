6f512edf607aa49b47519f2d05357b77e8fc9b8f
Ashwin Natarajan <ashwin.nat@gmail.com>
2025-05-30 02:24:52 +0530

    Update homepage layout and testimonials

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -5,21 +5,4 @@ export interface Testimonial {
   photoUrl?: string;
 }
 
-export const testimonials: Testimonial[] = [
-  {
-    name: "Katpadi Kumar",
-    title: "Monza T1 Enjoyer",
-    testimonial: "I like car.",
-  },
-  {
-    name: "Sarah K.",
-    title: "F1 Game Player",
-    testimonial: "Being able to compare my telemetry against rivals mid race is such an advantage!",
-    photoUrl: "https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg"
-  },
-  {
-    name: "James R.",
-    title: "Assetto Corsa Driver",
-    testimonial: "Being able to compare my telemetry with my teammates has transformed our practice sessions. We're all pushing each other to improve using the data.",
-  }
-];
+export const testimonials: Testimonial[] = [];
