5bfbe91edbd0ef660da2e7aec022893596c4896d
Ashwin Natarajan <ashwin.nat@gmail.com>
2025-06-21 04:29:40 +0530

    testimonials

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -5,4 +5,15 @@ export interface Testimonial {
   photoUrl?: string;
 }
 
-export const testimonials: Testimonial[] = [];
+export const testimonials: Testimonial[] = [
+  {
+    name: "Rage Nail",
+    title: "Content creator",
+    testimonial: "I use Pits n' Giggles mainly for the OBS stream overlay—makes it easy for my viewers to follow my lap times and help with consistency. The pace comparison overlay's been especially useful during races to get real-time feedback on where I'm gaining or losing time."
+  },
+  {
+    name: "Scurl",
+    title: "League admin",
+    testimonial: "The amount of overview Pits n' Giggles provides is an absolute gamechanger for me as a League Admin and Race Engineer. It combines extensive telemetry with a congenial design, which makes analyzing data as easy as it could get. It's simply a must have!",
+  }
+];
